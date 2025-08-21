a14c3fc9a52c5baf33aa69b5a09119f60a32a217
nawahtkui <alnwyra@gmail.com>
2025-08-21 22:21:16 +0300

    Update 1_Storage.sol

--- 1_Storage.sol
+++ 1_Storage.sol
@@ -1,29 +1,47 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.8.2 <0.9.0;
+pragma solidity ^0.8.20;
+
 
 /**
- * @title Storage
- * @dev Store & retrieve value in a variable
- * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
- */
+
+* @title Storage
+
+* @dev Store & retrieve a uint256 value
+
+*/
+
 contract Storage {
 
-    uint256 number;
+uint256 private number;
 
-    /**
-     * @dev Store value in variable
-     * @param num value to store
-     */
-    function store(uint256 num) public {
-        number = num;
-    }
 
-    /**
-     * @dev Return value 
-     * @return value of 'number'
-     */
-    function retrieve() public view returns (uint256){
-        return number;
-    }
+/**
+
+* @dev Store a value in `number`.
+
+* @param num value to store
+
+*/
+
+function store(uint256 num) external {
+
+number = num;
+
 }
+
+
+/**
+
+* @dev Return the current value of `number`.
+
+*/
+
+function retrieve() external view returns (uint256) {
+
+return number;
+
+}
+
+}
+
